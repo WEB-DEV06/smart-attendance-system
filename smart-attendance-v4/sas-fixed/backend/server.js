5a612ab576411a353269b468d86cb7ef94f43b67
WEB-DEV06 <fack20k@gmail.com>
2026-04-03 05:15:35 +0530

    Update server.js

--- smart-attendance-v4/sas-fixed/backend/server.js
+++ smart-attendance-v4/sas-fixed/backend/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173'],
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173'
   credentials: true
 }));
 app.use(express.json({ limit: '20mb' }));
