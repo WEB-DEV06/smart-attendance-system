8e3ca7d43225be5d4c0b573b55f0c601602f9bc8
WEB-DEV06 <fack20k@gmail.com>
2026-04-03 05:22:44 +0530

    Update server.js

--- smart-attendance-v4/sas-fixed/backend/server.js
+++ smart-attendance-v4/sas-fixed/backend/server.js
@@ -7,7 +7,6 @@ const app = express();
 
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5173'
-  credentials: true
 }));
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: true, limit: '20mb' }));
