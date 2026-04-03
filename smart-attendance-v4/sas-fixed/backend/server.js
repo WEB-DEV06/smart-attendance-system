2923f552473b4dcbc2cc100eadeb5b0327a21f3f
WEB-DEV06 <fack20k@gmail.com>
2026-04-03 12:18:33 +0530

    Update server.js

--- smart-attendance-v4/sas-fixed/backend/server.js
+++ smart-attendance-v4/sas-fixed/backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 const allowedOrigins = [
   process.env.FRONTEND_URL,
   'http://localhost:5173',
-  'https://smart-attendance-system-ten-gamma.vercel.app'
+  'https://smart-attendance-system-xi-three.vercel.app'
 ].filter(Boolean);
 
 app.use(cors({
